a61cb685e94e0c003a04ab892586960be0d6bee4
hoodini47 <msajdera@gmail.com>
2021-10-20 13:32:46 +0200

    Added: ACFs for 'Glaze Enviroment' Page.

--- enviroment-page-template.php
+++ enviroment-page-template.php
@@ -9,6 +9,24 @@
  $enviroment_title_1 = get_field("title_part_1");
  $enviroment_title_2 = get_field("title_part_2");
 
+ $section_header_1 = get_field("section_header_1");
+ $section_subheader_1 = get_field("section_subheader_1");
+ $section_content_1 = get_field("section_content_1");
+
+
+ $section_header_2 = get_field("section_header_2");
+ $section_subheader_2 = get_field("section_subheader_2");
+ $section_content_2 = get_field("section_content_2");
+
+ $section_header_3 = get_field("section_header_3");
+ $section_subheader_3 = get_field("section_subheader_3");
+ $section_content_3 = get_field("section_content_3");
+
+ $section_3_list_item_1 = get_field("section_3_list_item_1");
+ $section_3_list_item_2 = get_field("section_3_list_item_2");
+ $section_3_list_item_3 = get_field("section_3_list_item_3");
+
+
 get_header();
 ?>
 
@@ -23,7 +41,7 @@ get_header();
 
 						<div class="enviroment__title enviroment-page__title">
 
-								<h1><?php echo $enviroment_title_1 ?><span class="font-outline__red"><?php echo $enviroment_title_2 ?></span></h1>
+								<h1><?php echo $enviroment_title_1 ?> <span class="font-outline__red"><?php echo $enviroment_title_2 ?></span></h1>
 
 						</div>
 
@@ -43,15 +61,15 @@ get_header();
 							<div id="fixedTriangle" class="fixed-triangle--state-1">
 
 									<div class="fixed-triangle fixed-triangle--1">
-										<p data-section-pointer="#enviromentSection1">FABRICATION</p>
+										<p data-section-pointer="#enviromentSection1"><?php echo $section_header_1 ?></p>
 										<span></span>
 									</div>
 									<div class="fixed-triangle fixed-triangle--2">
-										<p data-section-pointer="#enviromentSection2">PRODUCTS</p>
+										<p data-section-pointer="#enviromentSection2"><?php echo $section_header_2 ?></p>
 										<span></span>
 									</div>
 									<div class="fixed-triangle fixed-triangle--3">
-										<p data-section-pointer="#enviromentSection3">TECHNOLOGY</p>
+										<p data-section-pointer="#enviromentSection3"><?php echo $section_header_3 ?></p>
 										<span></span>
 									</div>
 
@@ -61,36 +79,35 @@ get_header();
 						<div class="enviroment__sections">
 
 							<div id="enviromentSection1" class="enviroment__section">
-								<h3 class="text__red text__underline">FABRICATION</h3>
+								<h3 class="text__red text__underline"><?php echo $section_header_1 ?></h3>
 								<p>
-								<b>In the 21st century, we want to utilize 21st-century methods of fabrication.</b>
+								<b><?php echo $section_subheader_1 ?></b>
 								<br />
 								<br />
-								HP’s Multi Jet Fusion is a state-of-the-art cutting-edge technology that is giving us amazing precision and what we are looking for - consistency in the manufacturing process.
-								HP MJF doesn’t have a bad day, you can bet on it. 
+									<?php echo $section_content_1 ?>
 								</p>
 							</div>
 
 							<div id="enviromentSection2" class="enviroment__section">
-								<h3 class="text__red text__underline">PRODUCTS</h3>
+								<h3 class="text__red text__underline"><?php echo $section_header_2 ?></h3>
 								<p>
-								<b>Glaze product portfolio is a wide range of cutting-edge upper limb prosthetic devices.</b>
+								<b><?php echo $section_subheader_2 ?></b>
 								<br />
 								<br />
-								We have it all, trans-humeral and trans-radial passive, body-powered, and myoelectric prosthesis. All of the above-elbow devices are based on the revolutionary first in the world interchangeable elbow system. 
+									<?php echo $section_content_2 ?>
 								</p>
 							</div>
 
 							<div id="enviromentSection3" class="enviroment__section">
-								<h3 class="text__red text__underline">TECHNOLOGY</h3>
-								<p><b>We have few goals:</b></p>
+								<h3 class="text__red text__underline"><?php echo $section_header_3 ?></h3>
+								<p><b><?php echo $section_subheader_3 ?></b></p>
 								<ul>
-									<li><p><b>being always on time</b></p></li>
-									<li><p><b>delivering the best possible fit</b></p></li>
-									<li><p><b>go way beyond your and your patient expectations</b></p></li>
+									<li><p><b><?php echo $section_3_list_item_1 ?></b></p></li> 
+									<li><p><b><?php echo $section_3_list_item_2 ?></b></p></li>
+									<li><p><b><?php echo $section_3_list_item_3 ?></b></p></li>
 								</ul>
 								<p>
-									For this to happen we brought the machine learning and artificial intelligence algorithms to the table. Taking into consideration the alignment and all of the patient’s measurements our script is modeling the Glaze in real-time. This makes it possible to deliver dozens of devices to patients around the world in a matter of days.
+									<?php echo $section_content_3 ?>
 								</p>
 							</div>
 
